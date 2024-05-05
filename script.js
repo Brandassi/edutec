b0b7f713640e4bdb5a3236367b11069778a3d836
gustavo brandassi <gustavobrandassi@icloud.com>
2024-05-05 19:04:42 -0300

    ajuste banner, menu, e slides

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function() {
     var header = document.querySelector('header');
     var slider = document.querySelector('.slider').offsetTop; 
-    var isAboveSlider = window.scrollY < slider;
+    var isPastSlider = window.scrollY > slider;
 
-    header.classList.toggle('transparente', isAboveSlider);
+    header.classList.toggle('rolagem', isPastSlider);
 });
