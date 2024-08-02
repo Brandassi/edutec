21baef9a09f415fad70960f80dab306a78a67313
gustavo <gustavobrandassi@icloud.com>
2024-08-02 15:33:17 -0300

    mudei tudo para ser fiel ao figma

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function() {
-    var header = document.querySelector('#header');
-    var slider = document.querySelector('.slider').offsetTop;
-    var isPastSlider = window.scrollY > slider;
-
-    header.classList.toggle('rolagem', window.scrollY > 500);
-});
